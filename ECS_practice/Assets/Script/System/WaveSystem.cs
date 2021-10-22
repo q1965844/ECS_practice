77ff76f41c4c474a60d12d27077b6d6064ac458c
JingMin <q1965844@gmail.com>
2021-10-22 23:39:57 +0900

    【Job】Compare the one thread process and multi-threads process

--- ECS_practice/Assets/Script/System/WaveSystem.cs
+++ ECS_practice/Assets/Script/System/WaveSystem.cs
@@ -1,15 +1,40 @@
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Jobs;
 
-public class WaveSystem : ComponentSystem
+/// <summary>
+/// 只使用Main thread執行運算
+/// </summary>
+//public class WaveSystem : ComponentSystem
+//{
+//	protected override void OnUpdate()
+//	{
+//		////Jobified logic(sub thread)
+//		Entities.ForEach((ref Translation trans, ref MoveSpeed moveSpeed, ref WaveData waveData) =>
+//		{
+//			float zPosition = waveData.Amplitude * math.sin((float)Time.ElapsedTime * moveSpeed.value + trans.Value.x * waveData.XOffset + trans.Value.y * waveData.YOffset);
+//			trans.Value = new float3(trans.Value.x, trans.Value.y, zPosition);
+//		});
+//	}
+//}
+
+
+/// <summary>
+/// 使用Job System切分成Chunks並且由多個執行緒執行運算
+/// </summary>
+public class WaveSystem : SystemBase
 {
 	protected override void OnUpdate()
 	{
-		Entities.ForEach((ref Translation trans, ref MoveSpeed moveSpeed , ref WaveData waveData) =>
+		// main thread
+		var elapsedTime = (float)Time.ElapsedTime;
+
+		//Jobified logic(sub thread)
+		Entities.ForEach((ref Translation trans, ref MoveSpeed moveSpeed, ref WaveData waveData) =>
 		{
-			float zPosition = waveData.Amplitude * math.tan((float)Time.ElapsedTime * moveSpeed.value + trans.Value.x * waveData.XOffset + trans.Value.y * waveData.YOffset);
+			float zPosition = waveData.Amplitude * math.sin(elapsedTime * moveSpeed.value + trans.Value.x * waveData.XOffset + trans.Value.y * waveData.YOffset);
 			trans.Value = new float3(trans.Value.x, trans.Value.y, zPosition);
-		});
+		}).ScheduleParallel();
 	}
 }
